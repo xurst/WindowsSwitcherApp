1a3d15fc6a72e5224b48f3bd8ec1baa4a2e5b132
xurst <139590166+xurst@users.noreply.github.com>
2025-01-15 21:12:50 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Application = System.Windows.Application;
+using Forms = System.Windows.Forms;
+
+namespace WindowsApplicationSwiper
+{
+    public partial class App : Application
+    {
+        private WindowSpy? _windowSpy;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // Create debug spy form
+            _windowSpy = new WindowSpy();
+            _windowSpy.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _windowSpy?.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
